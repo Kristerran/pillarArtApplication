5d12bca01ec842bcef24e8bce71c102c847e8b90
Kris Baily <kristopherbaily@gmail.com>
2021-10-05 19:27:44 -0700

    Added start of views and static elements

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,7 @@
 const router = require('express').Router();
 const apiRoutes = require('./apiRoutes')
-router.use('/home', (req,res) => {
-    console.log(__dirname)
-    res.sendFile('C:/Users/krist/pillarArtApplication/views/index.html')
-})
+const homeRoutes = require('./homeRoutes.js')
+router.use('/', homeRoutes);
 router.use('/api', apiRoutes)
 
 module.exports = router
