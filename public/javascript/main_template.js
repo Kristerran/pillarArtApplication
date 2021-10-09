b0eaf83277f0a974db02b40ac0a62525a7950f0d
joe-toni <joefaburrieta@gmail.com>
2021-10-09 01:05:06 -0700

    adjusting styles on the main page display for smoother color scheme

--- public/javascript/main_template.js
+++ public/javascript/main_template.js
@@ -1,39 +1,25 @@
 var leftWall = document.querySelector(".left_frame_bar");
 var rightWall = document.querySelector(".right_frame_bar");
 
-//leftWall.addEventListener(onclick, shiftLeft);
-//rightWall.addEventListener(onclick, shiftRight);
+
 
 
 function shiftLeft()
 {
-    leftWall.style.borderLeft =  "300px solid teal";    
-    rightWall.style.borderRight = "100px solid teal"
-    
-   /* rightWall.style.height =  610;
-    rightWall.style.borderRight =  "10px solid teal";
-    rightWall.style.borderTop = "150px solid transparent";
-    rightWall.style.borderBottom = "130px solid transparent";*/
+    leftWall.style.borderLeft =  "300px solid  var(--darkestwhite )";    
+    rightWall.style.borderRight = "100px solid  var(--darkestwhite )";
 }
 
 function shiftRight()
 {
-
-    leftWall.style.borderLeft =  "100px solid teal"; 
-    rightWall.style.borderRight =  "300px solid teal";
-
+    leftWall.style.borderLeft =  "100px solid  var(--darkestwhite )"; 
+    rightWall.style.borderRight =  "300px solid  var(--darkestwhite )";
 }
 
 function center()
 {
-    {
-    
-        leftWall.style.borderLeft =  "200px solid teal";
-
-        rightWall.style.borderRight =  "200px solid teal";
-
-    }
-
+        leftWall.style.borderLeft =  "200px solid  var(--darkestwhite )";
+        rightWall.style.borderRight =  "200px solid  var(--darkestwhite )";
 }
 
 
